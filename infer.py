5ffaf05fcb75784a52a24375fcca04655c4918dd
Mixmix00 <mspier00001@gmail.com>
2024-04-17 23:02:32 -0700

    eq

--- infer.py
+++ infer.py
@@ -1,12 +1,11 @@
 from ultralytics import YOLO
 
 # Load a model
-# model = YOLO("yolov8n.yaml")  # build a new model from scratch
 model = YOLO("yolov8s_playing_cards.pt")  # load a pretrained model (recommended for training)
 
-# Use the model
-# results = model.train(data="coco128.yaml", epochs=3)  # train the model
-# results = model.val()  # evaluate model performance on the validation set
-results = model("assets/1.jpeg")  # predict on an image
-# success = YOLO("yolov8n.pt").export(format="onnx")  # export a model to ONNX format
-print([x for x in results])
+# Predict on an image
+
+results = model("/Users/maxspier/Documents/GitHub/Playing-Cards-Detection-with-YoloV8")
+
+print("START")
+print(results)
